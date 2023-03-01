509b2b7382c691bc5ba83d3b70816fee8abf6fe3
nedim-web <93007837+nedim-web@users.noreply.github.com>
2023-03-01 23:16:33 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const MCQRoute = require('./routes/MCQRoute');
+
+const app = express();
+app.use(express.json());
+
+const whiteList = ['https://nedim-SE.github.io'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use('/mcqs', MCQRoute);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(
+      process.env.PORT,
+      console.log('Connection established and server started')
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });

53c2b644a22ec1d1b47bd20abf0854df6b9fea5b
nedim-web <93007837+nedim-web@users.noreply.github.com>
2023-03-02 22:15:08 +0100

    Delete MCQRoute.js

--- routes/MCQRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getAllMcq, createMCQ} = require('../controllers/MCQController');
-
-router.get('/', getAllMcq)
-    .post('/', createMCQ)
-
-module.exports = router;

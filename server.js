40e4afee366f29ee7e56e763ca255b6f1bf02155
nedim-web <93007837+nedim-web@users.noreply.github.com>
2023-03-03 02:03:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const whiteList = ['https://nedim-se.github.io'];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) === -1) {
+    if (whiteList.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
